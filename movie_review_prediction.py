91b1c1930b5612ce1d86bf91a927513bd9c366ad
Avinash Gupta <avig1998@gmail.com>
2019-04-09 13:11:47 -0400

    Update movie_review_prediction.py

--- movie_review_prediction.py
+++ movie_review_prediction.py
@@ -1,132 +1,90 @@
-# Avinash Gupta
-# G01009180
-# CS 484 002
-# Project 1
-# This is a project that reads a files containing movie reviews and then accepts a new review and determines whether
-# it is a good or bad review by using KNN
 import string
-import nltk
-import sklearn
 from nltk.corpus import stopwords
-from nltk.stem import LancasterStemmer
+from nltk.stem import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
-import scipy as sp
+from collections import Counter
 from sklearn.metrics.pairwise import cosine_similarity
-
-correct_prediction = 0
-nearest_labels = []
-
-
-def knn_sentiment(train_reviews, test_reviews, oneslabels):
-
-	cos_similarity = cosine_similarity(train_reviews,test_reviews).flatten()
-	c = np.asarray(cos_similarity)
-	i = 0
-
-	while i < len(cos_similarity):
-
-		nearest_labels.append(oneslabels[i])
-		i += 1
-		
-	i = 0
-	#nearest_labels.sort(reverse=True)
-	#print(len(nearest_labels))
-	
-	return nearest_labels
-	
-review_file = open("train.txt", "r",encoding="utf-8") #opens the file
-test_file = open("test.txt", "r",encoding="utf-8") #opens the test file
-labels_file = open("labels.txt","w",encoding="utf-8")
-
-reviews = review_file.readlines() #reads all the lines
-test_reviews = test_file.readlines() #reads all the lines
-
-stop_words = set(stopwords.words('english')) #gets the list of stop words defined in the library and stores it in the list named stop_words
+import os
+from sklearn.metrics import accuracy_score
 
 
-tab = str.maketrans("", "", string.punctuation) #This makes a translation table. It takes 3 parametrs, the first is a string that specifies the characters that need to be replaced
-												  #The second is a string that contains the characters that replace the characters that need to be replaced
-												  #The thrid is a string that contains characters that need to be deleted
-												  #In this case, we want to remove the punctuations
+def knn_sentiment(training_idf, testing_vector, train_labels, k=1000):
+    nearest_labels = []
+    cos_mat = cosine_similarity(testing_vector, training_idf)  # makes a cosine similarity matrix
+    i = 0
 
-stemmer = LancasterStemmer()
+    while i < cos_mat.shape[0]:
+        sample = cos_mat[i]  # stores the pair of the ith testing review with all the training reviews
+        sample = np.argsort(sample)  # sorts the list through the indices
+        sample = sample[::-1]  # flips the list so it looks like descending order
+        index = sample[:k]  # gets the k nearest labels
+        most_occuring_label = Counter(index)
+        nearest_labels.append(train_labels[most_occuring_label.most_common(1)[0][0]])
+        i += 1
+    return nearest_labels
+
+train_file = open("training_reviews.txt", "r", encoding="utf-8")
+predicted_file = open("labels.txt", "w")
+train = train_file.readlines()  # reads all the lines in the training file
+
+stopwords = set(stopwords.words('english'))  # gets the list of stop words
+
+tab = str.maketrans("", "", string.punctuation)  # removes all the punctuation
+
 doc_vector = TfidfVectorizer()
+doc_stemmer = PorterStemmer()
 
 labels = []
-i=0
+i = 0
 
+while i < len(train):  # cleans the training reviews
 
-while i<len(reviews):
+    labels.append(train[i].split()[0])  # splits the review and stores the label of that review in the list
+    train[i] = train[i].translate(tab)  # removes the punctuation
+    train[i] = train[i].replace("<br \>", "")  # removes the break tag
+    train[i] = train[i].replace("EOF", "")  # removes the EOF marker
+    train[i] = train[i].replace("1", "")  # removes the label of the review
 
-	labels.append(reviews[i].split()[0]) #stores the label of that review
-	
-	reviews[i] = reviews[i].translate(tab)
-	reviews[i] = reviews[i].replace("<br />", "")
-	reviews[i] = reviews[i].replace("EOF", "")
-	reviews[i] = reviews[i].replace("1", "")
-	
-	
-	test_reviews[i] = test_reviews[i].translate(tab)
-	test_reviews[i] = test_reviews[i].replace("<br />", "")
-	test_reviews[i] = test_reviews[i].replace("EOF", "")
-	
-	copy = ""
-	words = reviews[i].split()
-	
-	j=0
-	
-	while j<len(words):
-		
-		if words[j].lower() in stop_words:
-			
-			words.remove(words[j])
-		else:
-			copy += " " + words[j]#stemmer.stem(words[j])  
-		j += 1
-	#print("Training data: " + copy)
-	reviews[i] = copy
-	
-	j=0
-	
-	copy = ""
-	words = test_reviews[i].split()
-	
-	while j<len(words):
-		
-		if words[j].lower() in stop_words:
-			
-			words.remove(words[j])
-		else:
-			copy += " " + words[j] #stemmer.stem(words[j])  
-		j += 1
-	#print("Testing data: " + copy)	
-	test_reviews[i] = copy
-	i += 1
+    words = train[i].split()
+    copy = ""
 
-#vocab_matrix = np.concatenate((reviews,test_reviews), axis=0)
-training = doc_vector.fit_transform(reviews)
+    j = 0
 
-i=0
-accuracy_labels = []
+    while j < len(words):
+        if words[j].lower() in stopwords:
+            words.remove(words[j])
+        else:
+            copy += words[j] + " "  # removes all the stop words and makes a new string
+        j += 1
 
-while i<len(test_reviews):
-	
-	test_array = []
-	test_array.append(test_reviews[i]);
-	testing = doc_vector.transform(test_array)
-	temp = knn_sentiment(training,testing,labels)
-	accuracy_labels.append(max(temp))
-	i+= 1
+    train[i] = copy  # replaces the cleaned line into the review list
+    i += 1
 
-	
-i=0
-print(len(accuracy_labels))
-while(i<25000):
+i = 0
 
-	labels_file.write(accuracy_labels[i] + "\n")
-	i+=1
-	
-review_file.close()
-test_file.close()
-labels_file.close()
+divider = (2 * len(train)) // 3
+
+training_half = train[:divider - 1]
+training_labels = labels[:divider - 1]
+
+testing_half = train[divider:]
+testing_labels = labels[divider:]
+
+training = doc_vector.fit_transform(training_half)  # converts the training reviews to a tf idf matrix
+testing = doc_vector.transform(testing_half)  # converts the test reviews to a tf idf matrix
+
+predicted_labels = knn_sentiment(training, testing, labels)
+
+i = 0
+
+accuracy = accuracy_score(testing_labels, predicted_labels)
+
+while i < len(predicted_labels):
+    predicted_file.write(predicted_labels[i] + "\n")
+    i += 1
+
+print("The number of accuracy of the model is " + str(accuracy*100))
+print("project completed")
+train_file.close()
+predicted_file.close()
